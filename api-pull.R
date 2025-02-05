b49eeafe1b8cc8215ec323fdc4a27d7e52f38239
Michael Hanley <152537546+michaeljhanley@users.noreply.github.com>
2025-02-04 22:36:55 -0800

    Pulling API data into a CSV to avoid unnecessary calls

--- api-pull.R
+++ api-pull.R
@@ -1,5 +1,9 @@
 library(epidatr)
+library(readr)
 
 data <- pub_fluview(regions = c("hhs1", "hhs2", "hhs3", "hhs4", "hhs5", "hhs6", "hhs7", "hhs8", "hhs9", "hhs10"), epiweeks = epirange(201440, 201922))
 
 print(data)
+
+# Writing raw data to CSV to avoid unnecessary API calls - remove later
+write_csv(data, "raw_test_data.csv")
